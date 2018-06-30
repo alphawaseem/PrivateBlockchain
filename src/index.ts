1bbab806d97f4d4c32d6191decdee8ac37cb628f
Waseem Ahmed <codewaseem@gmail.com>
2018-06-30 19:31:30 +0530

    create genesis block

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,7 @@ import Blockchain from "./models/Blockchain";
 (async () => {
     const blockchain = new Blockchain();
     await blockchain.createAndAddBlock("First block of this blockchain - the genesis block");
-    await blockchain.createAndAddBlock("block 1");
-    await blockchain.createAndAddBlock("block 2");
-
-
+    
     console.log(await blockchain.getChain());
 
     blockchain.validateChain();
