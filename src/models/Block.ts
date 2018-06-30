990041f956f816d8b3a4d8d9b9e3821a127da60f
Waseem Ahmed <codewaseem@gmail.com>
2018-06-30 15:16:27 +0530

    implement Block class

---
+++ src/models/Block.ts
@@ -0,0 +1,17 @@
+import { SHA256, WordArray } from "crypto-js";
+
+export default class Block {
+    hash: string;
+    height: number;
+    body: any;
+    time: string;
+    previousBlockHash: string;
+
+    constructor(height: number, data: any, previousBlockHash: string) {
+        this.height = height;
+        this.body = data;
+        this.time = new Date().getTime().toString().slice(0, -3);
+        this.previousBlockHash = previousBlockHash;
+        this.hash = SHA256(JSON.stringify(this)).toString();
+    }
+}
